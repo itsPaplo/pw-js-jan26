28f759615c66c2d8d8cc89d01e7b2cb741867d1d
Philip <thisispaplo@gmail.com>
2026-08-09 03:03:12 +0530

    Initial commit

---
+++ Week1Day1/Day1Assignment.js
@@ -0,0 +1,29 @@
+/* Create the following variables using let (not using var) and check their typeOf
+a) firstName
+b) companyName
+c) mobileNumber
+d) isAutomation
+e) hasPlaywright (do not assign)
+Print and confirm the values and data types */
+
+let firstName = "Phill";
+let companyName = "TLf";
+let mobileNumber = 7118181618;
+let isAutomation = false;
+let hasPlaywright;
+
+//Print and confirm the values and data types
+console.log(firstName);
+console.log(typeof firstName);
+
+console.log(companyName);
+console.log(typeof companyName);
+
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
